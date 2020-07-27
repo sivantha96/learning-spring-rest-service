1166b6ffa364eecf8567afbaf423c90ca5230de3
Sivantha Paranavithana <madusham3@gmail.com>
2020-07-27 14:37:26 +0530

    Initial commit

---
+++ src/main/java/com/sivantha96/learningspringrestservice/LearningSpringRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sivantha96.learningspringrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sivantha96/learningspringrestservice/LearningSpringRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivantha96.learningspringrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
